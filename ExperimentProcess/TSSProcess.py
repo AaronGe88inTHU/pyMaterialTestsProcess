1c5608352f187c88ddb56ec7550790fcdba6cb1a
37819541+AaronGe88inTHU@users.noreply.github.com <37819541+AaronGe88inTHU@users.noreply.github.com>
2018-11-03 18:30:27 +0800

    v3

--- ExperimentProcess/TSSProcess.py
+++ ExperimentProcess/TSSProcess.py
@@ -6,56 +6,21 @@
 
 import numpy as np
 from ReadFile import readTestInfo
-
+import pandas as pd
 
 # In[3]:
 
+def main(argv):
+    fileName = argv[1]
+    direction = argv[2]
+    up = readTestInfo(fileName, 'up.csv', direction)
+    down = readTestInfo(fileName, 'down.csv', direction)
 
-up = readTestInfo('dows_11.xlsx', 'up.csv', 'u_c')
-
-
-# In[5]:
-
-
-down = readTestInfo('dows_11.xlsx', 'down.csv', 'u_c')
-
-
-# In[8]:
-
-
-stroke = - up[:,1]+down[:,1]
-
-
-# In[21]:
-
-
-stroke
-
-
-# In[15]:
-
-
-result = np.hstack([stroke.reshape(-1,1), up[:,-1].reshape(-1,1)])
-
-
-# In[16]:
-
-
-result.shape
-
-
-# In[20]:
-
-
-from matplotlib import pyplot as plt
-plt.plot(result[:,0], result[:,1])
-plt.show
-
-
-# In[23]:
-
-
-import pandas as pd
-df = pd.DataFrame(result, columns=['stroke', 'force'])
-df.to_excel('sf.xlsx','1')
+    stroke = - up[:,1]+down[:,1]
+    result = np.hstack([stroke.reshape(-1,1), up[:,-1].reshape(-1,1)])
+    #from matplotlib import pyplot as plt
+    #plt.plot(result[:,0], result[:,1])
+    #plt.show
+    df = pd.DataFrame(result, columns=['stroke', 'force'])
+    df.to_excel('sf.xlsx','1')
 
