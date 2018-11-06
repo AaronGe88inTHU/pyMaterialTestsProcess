356f426f57d678e3f3d9977407a0f9ee424d444b
37819541+AaronGe88inTHU@users.noreply.github.com <37819541+AaronGe88inTHU@users.noreply.github.com>
2018-11-06 22:36:12 +0800

    GISSIMO2

--- ExperimentProcess/GISSIMO/FileOperate.py
+++ ExperimentProcess/GISSIMO/FileOperate.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import logging
+import os
+from scipy import interpolate
 
 def readXlsx(fileName):
     """
@@ -14,11 +16,45 @@ def readXlsx(fileName):
     etaThetaEpsilon = []
     for sheet in xlsx.sheet_names:
         df = pd.read_excel(xlsx, sheet)
-        eta = df.ix[:, 'eta'].values.reshape(-1, 1)
-        thetaBar = df.ix[:, 'thetaBar'].values.reshape(-1, 1)
-        eplison = df.ix[:, 'epsilon'].values.reshape(-1, 1)
+        eta = df.loc[:, 'TRI'].values.reshape(-1, 1)
+        thetaBar = df.loc[:, 'LP'].values.reshape(-1, 1)
+        eplison = df.loc[:, 'EPS'].values.reshape(-1, 1)
         ete = np.hstack([eta, thetaBar, eplison])
         etaThetaEpsilon.append(ete)
     
     logging.debug(etaThetaEpsilon)
     return np.array(etaThetaEpsilon)
+
+def preProcess(cwd):
+    fileNames = os.listdir(cwd)
+    clearedData = pd.ExcelWriter('clearedData.xlsx')
+    for f in fileNames:
+        if os.path.splitext(f)[1] == '.xlsx':
+            ff = os.path.join('data', f)
+            df = pd.read_excel(ff, 'Sheet1')
+            time = df.loc[:,'TIME'].values[1:]
+            time1 = df.loc[:, 'TIME.1'].values[1:]
+            
+            time = np.array(time)[~np.isnan(time)]
+            time1 = np.array(time1)[~np.isnan(time1)]
+            
+            (timeEps, timeLP) = (time, time1) if len(time) < len(time1) else (time1, time)
+            eps = df.loc[:, 'EPS'].values[1:]
+            eps = np.array(eps)[~np.isnan(eps)]
+            
+            #timeLP = np.array(timeLP)[np.where(timeLP < np.max(timeEps))]
+            #wprint(timeLP)
+            timeLP = timeLP[timeLP < np.max(timeEps)]
+            funcInterp = interpolate.interp1d(timeEps, eps, bounds_error=None)
+            epsFullLength = funcInterp(timeLP).reshape(-1,1)
+            timeLP = timeLP.reshape(-1,1)
+            LP = df.loc[:,'LP'].values[1:]
+            LP = LP[0:len(timeLP)].reshape(-1,1)
+            
+            TRI = df.loc[:,'TRI'].values[1:]
+            TRI = TRI[0:len(timeLP)].reshape(-1, 1)
+
+            data = np.hstack([timeLP, epsFullLength, LP, TRI])
+            dff = pd.DataFrame(data, columns=['TIME', 'EPS', 'LP', 'TRI'])
+            dff.to_excel(clearedData, sheet_name=os.path.splitext(f)[0])
+    clearedData.save()
