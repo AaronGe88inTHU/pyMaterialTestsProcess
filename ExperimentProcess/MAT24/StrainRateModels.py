57af38001dd618ca7bd8b65f9db57022e7b9e7ee
AaronGe88inTHU <37819541+AaronGe88inTHU@users.noreply.github.com>
2020-02-12 17:24:49 +0800

    JCModel add

--- ExperimentProcess/MAT24/StrainRateModels.py
+++ ExperimentProcess/MAT24/StrainRateModels.py
@@ -6,98 +6,164 @@ import PrepareEngineeringCurve
 def J_C(x0, plastic_stress_curves):
     A, B, n, c = x0 
     sr = {}
-    if '1' in plastic_stress_curves.keys():
-        stress = plastic_stress_curves['1']
-        plastic_strain = stress[:, 0]
-        rate1 = stress[:, 1]
-    else:
-        k = [ k for k in plastic_stress_curves.keys()][0]
-        plastic_strain = plastic_stress_curves[k][:, 0]
-        rate1 = np.array(A + B * np.power(plastic_strain, n))
+    # if '1' in plastic_stress_curves.keys():
+    #     # stress = plastic_stress_curves['1']
+    #     # plastic_strain = stress[:, 0]
+    #     # rate1 = stress[:, 1]
+    #     pass
+    # else:
+    k = [ k for k in plastic_stress_curves.keys()][0]
+    plastic_strain = plastic_stress_curves[k][:, 0]
+    rate1 = np.array(A + B * np.power(plastic_strain, n))
 
-    print(plastic_strain, rate1)
+    #print(plastic_strain, rate1)
     for r, _ in plastic_stress_curves.items():
         #try:
             sr[r] = np.hstack([plastic_strain.reshape(-1,1), (rate1 * (1 + c * np.log(float(r)))).reshape(-1,1)])
 
     return sr
 
+def C_S(x0, plastic_stress_curves):
+    A, B, n, d1, d2 = x0 
+    sr = {}
+    # if '1' in plastic_stress_curves.keys():
+    #     # stress = plastic_stress_curves['1']
+    #     # plastic_strain = stress[:, 0]
+    #     # rate1 = stress[:, 1]
+    #     pass
+    # else:
+    k = [ k for k in plastic_stress_curves.keys()]
+    min_k = np.min([float(kk) for kk in k ])
+    plastic_strain = plastic_stress_curves[str(min_k)][:, 0]
+    rate1 = np.array(A + B * np.power(plastic_strain, n))
+    #rate_low = plastic_stress_curves[str]    
+    
+
+    #print(plastic_strain, rate1)
+    for r, _ in plastic_stress_curves.items():
+        #try:
+            sr[r] = np.hstack([plastic_strain.reshape(-1,1), ( rate1 * (1 + np.power((float(r) * d1), d2))).reshape(-1,1)])
+    return sr
+
+# def Double_C_S(x0, plastic_stress_curves):
+#     pass
+
+
 def J_C_Model(x0, plastic_stress_curves):
     A, B, n, c = x0 
     #print(plastic_stress_curves.keys())
-    if '1' in plastic_stress_curves.keys():
-        stress = plastic_stress_curves['1']
-        rate1 = stress[:, 1]
-    else:
-        k = [ k for k in plastic_stress_curves.keys()][0]
-        plastic_strain = plastic_stress_curves[k][:, 0]
-        rate1 = np.array(A + B * np.power(plastic_strain, n))
-
     err = []
-
+    # if '1' in plastic_stress_curves.keys():
+    #     stress = plastic_stress_curves['1']
+    #     rate1 = stress[:, 1]
+    #     for r, s in plastic_stress_curves.items():
+    #         if not r == "1":
+    #             sr = rate1 * (1 + c * np.log(float(r)))
+    #             assert len(s) == len(sr)
+    #             err.extend(s[:, 1] - sr)
+    # else:
+    k = [ k for k in plastic_stress_curves.keys()][0]
+    plastic_strain = plastic_stress_curves[k][:, 0]
+    rate1 = np.array(A + B * np.power(plastic_strain, n))
     for r, s in plastic_stress_curves.items():
         #try:
-            sr = rate1 * (1 + c * np.log(float(r)))
-            assert len(s) == len(sr)
-            err.extend(s[:, 1] - sr)
+        sr = rate1 * (1 + c * np.log(float(r)))
+        assert len(s) == len(sr)
+        err.extend(s[:, 1] - sr)
+    
         #except Exception as identifier:
         #    pass
     #print(err)
     return np.array(err)
 
-class StrainRateSenstivity:
-    def __init__(self):
-        #self.xlsx_file = xlsx_file
-        self.eng_curve_dict = {} # Eng strain-stress curves at strain rates
-        
+def C_S_Model(x0, plastic_stress_curves):
+    A, B, n, d1, d2 = x0 
+    #print(plastic_stress_curves.keys())
+    err = []
+    # if '1' in plastic_stress_curves.keys():
+    #     stress = plastic_stress_curves['1']
+    #     rate1 = stress[:, 1]
+    #     for r, s in plastic_stress_curves.items():
+    #         if not r == "1":
+    #             sr = rate1 * (1 + c * np.log(float(r)))
+    #             assert len(s) == len(sr)
+    #             err.extend(s[:, 1] - sr)
+    # else:
+    k = [ k for k in plastic_stress_curves.keys()][0]
+    plastic_strain = plastic_stress_curves[k][:, 0]
+    rate1 = np.array(A + B * np.power(plastic_strain, n))
+    for r, s in plastic_stress_curves.items():
+        #try:
+        sr = rate1 * (1 + np.power(float(r) * d1, d2))
+        assert len(s) == len(sr)
+        err.extend(s[:, 1] - sr)
     
-    def read_file(self, xlsx_file):
-        xlsx = pd.ExcelFile(xlsx_file)
-        names = xlsx.sheet_names
-        for name in names:
-            df = pd.read_excel(xlsx, name)
-            #df.replace('--',  0)
-            self.eng_curve_dict[name] = df.values
+        #except Exception as identifier:
+        #    pass
+    #print(err)
+    return np.array(err)
 
-    def to_true_curves(self):
-        self.plastic_curve_dic = {}
-        if len(self.eng_curve_dict) == 0:
-            return None
-            #return None
-        else:
-            
-            for k, v in self.eng_curve_dict.items():
-                self.plastic_curve_dic[k] = PrepareEngineeringCurve.eng2True(v)
+# def Double_C_S_Model(x0, plastic_stress_curves):
+#     A, B, n, d1, d2 = x0 
+#     #print(plastic_stress_curves.keys())
+#     err = []
+#     # if '1' in plastic_stress_curves.keys():
+#     #     stress = plastic_stress_curves['1']
+#     #     rate1 = stress[:, 1]
+#     #     for r, s in plastic_stress_curves.items():
+#     #         if not r == "1":
+#     #             sr = rate1 * (1 + c * np.log(float(r)))
+#     #             assert len(s) == len(sr)
+#     #             err.extend(s[:, 1] - sr)
+#     # else:
+#     k = [ k for k in plastic_stress_curves.keys()][0]
+#     plastic_strain = plastic_stress_curves[k][:, 0]
+#     rate1 = np.array(A + B * np.power(plastic_strain, n))
+#     for r, s in plastic_stress_curves.items():
+#         #try:
+#         sr = rate1 * (1 + np.power(float(r) * d1, d2))
+#         assert len(s) == len(sr)
+#         err.extend(s[:, 1] - sr)
+    
+#         #except Exception as identifier:
+#         #    pass
+#     #print(err)
+#     return np.array(err)
 
-        return self.plastic_curve_dic
-            
+
+
+class StrainRateSenstivity:
+    def __init__(self, plastic_curve_dic):
+        #self.xlsx_file = xlsx_file
+        #self.eng_curve_dict = {} # Eng strain-stress curves at strain rates
+        self.plastic_curve_dic = plastic_curve_dic
     def fit_model(self, model, x0):
         if not 'plastic_curve_dic' in self.__dict__:
         
             return None
         else:
-            values = self.plastic_curve_dic.values()
+            #values = self.plastic_curve_dic.values()
 
-            idx = min([value.shape[0] for value in values])
+           
             #for k, v in self.plastic_curve_dic.items():
-            stress_curves = {}
-            for k, v in self.plastic_curve_dic.items():
-                stress_curves[k] = v[:idx, :]
-
-
-            self.res = leastsq(model, x0, args=stress_curves)
+            #stress_curves = {}
+            #for k, v in self.plastic_curve_dic.items():
+            #    stress_curves[k] = v[idxmin:idxmax, :]
+            self.res = leastsq(model, x0, args=self.plastic_curve_dic)
             
-            curves = J_C(self.res[0], self.plastic_curve_dic)
+           
             
-            return self.res, curves
-            
+            return self.res
+    def to_curves(self, func):
+        new_curves = func(self.res[0], self.plastic_curve_dic)
+        return new_curves
 
-
-m980 = StrainRateSenstivity()
-m980.read_file('980.xlsx')
-curves = m980.to_true_curves()
-res, new_curves = m980.fit_model(J_C_Model, [1000, 200, 0.1, 1])
-print(new_curves)
+curves = PrepareEngineeringCurve.read_plastic_file('GC420.xlsx')
+GC420 = StrainRateSenstivity(curves)
+#curves = m980.to_true_curves()
+res = GC420.fit_model(C_S_Model, [1000, 200, 0.1, 1.0001, 1])
+new_curves = GC420.to_curves(C_S)
+print(res)
 from matplotlib import pyplot as plt
 
 for k, y in curves.items():
